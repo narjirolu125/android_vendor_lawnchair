3b6f9b71789a7688e40d7b1befd879c42a8a5b6b
N A R G <narjirolu@gmail.com>
2020-04-17 00:40:04 +0700

    Update Android.mk

--- priv-app/Lawnchair/Android.mk
+++ priv-app/Lawnchair/Android.mk
@@ -6,7 +6,8 @@ LOCAL_MODULE_STEM := Lawnchair.apk
 LOCAL_SRC_FILES := Lawnchair.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app/LawnchairLOCAL_OVERRIDES_PACKAGES := ParanoidQuickStep Launcher3QuickStep PixelLauncher TrebuchetQuickStep
+LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app/Lawnchair
+LOCAL_OVERRIDES_PACKAGES := ParanoidQuickStep Launcher3QuickStep PixelLauncher TrebuchetQuickStep
 
 
 include $(BUILD_PREBUILT)
