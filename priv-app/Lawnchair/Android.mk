9a5eede1daa87e870d92fa44e56883f54893696f
N A R G <narjirolu@gmail.com>
2020-04-17 00:03:35 +0700

    Lawnchair: Override other launchers

--- priv-app/Lawnchair/Android.mk
+++ priv-app/Lawnchair/Android.mk
@@ -6,6 +6,7 @@ LOCAL_MODULE_STEM := Lawnchair.apk
 LOCAL_SRC_FILES := Lawnchair.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app/Lawnchair
+LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app/LawnchairLOCAL_OVERRIDES_PACKAGES := ParanoidQuickStep Launcher3QuickStep PixelLauncher TrebuchetQuickStep
+
 
 include $(BUILD_PREBUILT)
